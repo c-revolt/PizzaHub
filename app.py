46d6d0ee475718e2b48fa008d33112d0b158f5b6
Alexander Pride <a.pride@Alexanders-MacBook-Air.local>
2024-03-14 08:00:25 +0300

    lesson_2_Router

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+import os
 
-bot = Bot(token="6303439361:AAHwY8EBbWq-1MLfu5noHautpcbvFDDmpK0")
+from aiogram import Bot, Dispatcher
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+from handlers.user_private import user_private_router
+
+ALLOWED_UPDATES=['message', 'edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message) -> None:
-    await message.answer('О, привет! Ты нажал на коману СТАРТ!')
-
-@dp.message()
-async def echo(message: types.Message) -> None:
-    await message.answer(message.text)
+dp.include_router(user_private_router)
 
 
 async def main() -> None:
-    await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
