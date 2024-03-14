bdfcfea3e00cd457ddabfaac3028e9aa76082b8c
Alexander Pride <a.pride@Alexanders-MacBook-Air.local>
2024-03-14 12:49:28 +0300

    lesson_3_filters_and_new_commands

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,8 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='menu', description='Показать меню'),
+    BotCommand(command='about', description='О нас'),
+    BotCommand(command='payment', description='Оплата'),
+    BotCommand(command='shipping', description='Доставка')
+]
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,12 +1,30 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart, Command, or_f
+
+from filters.chat_types import ChatTypeFilter
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message) -> None:
     await message.answer('Привет! Я виртуальный помощник в нашей пиццерии!')
 
-@user_private_router.message(Command('menu'))
+@user_private_router.message(or_f(Command('menu'), (F.text.lower() == "меню")))
 async def menu_cmd(message: types.Message) -> None:
     await message.answer('Вот наше меню:')
+
+@user_private_router.message(F.text.lower() == 'о нас')
+@user_private_router.message(Command('about'))
+async def about_cmd(message: types.Message):
+    await message.answer('Информация о нас....')
+
+@user_private_router.message((F.text.lower().contains('плат')) | (F.text.lower() == 'способы оплаты'))
+@user_private_router.message(Command('payment'))
+async def payment_cmd(message: types.Message):
+    await message.answer('Способы оплаты: ')
+
+@user_private_router.message((F.text.lower().contains('доставк')) | (F.text.lower() == 'варианты доставки'))
+@user_private_router.message(Command('shipping'))
+async def shipping_cmd(message: types.Message):
+    await message.answer('Варианты доставки: ')
