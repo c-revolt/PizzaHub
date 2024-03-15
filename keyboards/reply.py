5896a9cb8b2190c0fab953f0aa662c9414a47d35
Alexander Pride <a.pride@Alexanders-MacBook-Air.local>
2024-03-15 12:03:24 +0300

    lsn-6: fsm and admin commands

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,18 +1,28 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import KeyboardButton, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-start_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text='Меню'),
-            KeyboardButton(text='О нас'),
-        ],
-        [
-            KeyboardButton(text='Способы оплаты'),
-            KeyboardButton(text='Варианты доставки')
-        ]
-    ],
-    resize_keyboard=True, 
-    input_field_placeholder='Что вас интересует?'
-)
+
+def get_keyboard(
+    *btns: str,
+    placeholder: str = None,
+    request_contact: int = None,
+    request_location: int = None,
+    sizes: tuple[int] = (2,),
+
+):
+    keyboard = ReplyKeyboardBuilder()
+
+    for index, text in enumerate(btns, start=0):
+
+        if request_contact and request_contact == index:
+            keyboard.add(KeyboardButton(text=text, request_contact=True))
+
+        elif request_location and request_location == index:
+            keyboard.add(KeyboardButton(text=text, request_location=True))
+        else:
+            keyboard.add(KeyboardButton(text=text))
+
+    return keyboard.adjust(*sizes).as_markup(
+        resize_keyboard=True, input_field_placeholder=placeholder)
 
 del_kb = ReplyKeyboardRemove()
